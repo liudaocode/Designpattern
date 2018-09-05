f95f4b126180827842b6ff4f7d8051260ebe5494
liudaocode <18260346217@163.com>
2018-09-05 21:32:48 +0800

    简单工厂模式适用情况包括：工厂类负责创建的对象比较少；客户端只知道传入工厂类的参数，对于如何创建对象不关心。

---
+++ calculator/Operation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace calculator
+{
+   public class Operation
+    {
+        public double NumberA { get; set; }
+        public double NumberB { get; set; }
+
+        public virtual double GetResult()
+        {
+            double result = 0;
+            return result;
+        }
+    }
+
+    class OperationAdd : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA + NumberB;
+            return result;
+        }
+
+    }
+    class OperationSub : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA - NumberB;
+            return result;
+        }
+
+    }
+    class OperationMul : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA * NumberB;
+            return result;
+        }
+
+    }
+    class OperationDiv : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            if (NumberB == 0) throw new Exception("除数不可为0！");
+            result = NumberA / NumberB;
+            return result;
+        }
+
+    }
+}
--- calculator/Program.cs
+++ calculator/Program.cs
@@ -9,6 +9,25 @@ namespace calculator
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("请输入第一个数");
+                double numA = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("请输入操作符");
+                string operate = Console.ReadLine();
+                Console.WriteLine("请输入第二个数");
+                double numB = Convert.ToDouble(Console.ReadLine());
+                Operation oper = OperationFactory.CreateOperate(operate);
+                oper.NumberA = numA;
+                oper.NumberB = numB;
+                Console.WriteLine(oper.GetResult());
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
         }
     }
 }
